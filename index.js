3a0abc82e1497eb0d8cb9463fb57cff7fff3e7b8
zer0nim <marinernes@hotmail.fr>
2018-04-17 11:40:05 +0200

    rot42 decode_rot42 commands

--- index.js
+++ index.js
@@ -39,11 +39,25 @@ app.get('/auth/redirect', (req, res) =>{
 });
 
 app.post('/rot42', (req, res) =>{
+	colors = ["f44336", "e91e63", "9c27b0", "673ab7", "3f51b5", "2196f3", "03a9f4", "00bcd4", "009688", "4caf50", "8bc34a", "cddc39", "ffeb3b", "ffc107", "ff9800", "ff5722"];
 	var text = req.body.text;
-	res.send(rot(text, 42));
+
+	let data = {
+	  response_type: 'in_channel', // public to the channel
+	  attachments:[ {
+	    image_url: 'https://dummyimage.com/600x400/' + colors[ Math.floor(Math.random() * colors.length) ] + '/fff&text=' + rot(text, 42)
+	  } ]};
+	res.json(data);
 });
 
 app.post('/decode_rot42', (req, res) =>{
+	colors = ["f44336", "e91e63", "9c27b0", "673ab7", "3f51b5", "2196f3", "03a9f4", "00bcd4", "009688", "4caf50", "8bc34a", "cddc39", "ffeb3b", "ffc107", "ff9800", "ff5722"];
 	var text = req.body.text;
-	res.send(rot(text, 26 - 42 % 26));
+
+	let data = {
+	  response_type: 'in_channel', // public to the channel
+	  attachments:[ {
+		image_url: 'https://dummyimage.com/600x400/' + colors[ Math.floor(Math.random() * colors.length) ] + '/fff&text=' + rot(text, 26 - 42 % 26)
+	  } ]};
+	res.json(data);
 });
